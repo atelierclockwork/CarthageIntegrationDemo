f7345897b7501cce14e6ec38df8d0aa844ed5846
Michael Skiba <mike.skiba@me.com>
2015-04-27 22:09:37 -0400

    Initial Commit
    xCode Project Creation - Next commit will be delta from Carthage integration

---
+++ CarthageIntegrationDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  CarthageIntegrationDemo
+//
+//  Created by Michael Skiba on 4/27/15.
+//  Copyright (c) 2015 Atelier Clockwork. All rights reserved.
+//
+
+#import "ViewController.h"
+@import PodsVSCarthage;
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *demoLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.demoLabel.text = ExternalDetail.publicDetail;
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
